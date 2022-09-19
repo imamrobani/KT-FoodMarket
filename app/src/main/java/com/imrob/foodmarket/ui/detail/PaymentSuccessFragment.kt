22876e134fbd866e21a11e4d3e0d5baf8420c5b2
Imam Robani <imamrob@gmail.com>
2022-09-19 08:37:42 +0700

    feat: add fragment success payment

---
+++ app/src/main/java/com/imrob/foodmarket/ui/detail/PaymentSuccessFragment.kt
@@ -0,0 +1,18 @@
+package com.imrob.foodmarket.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.imrob.foodmarket.R
+
+class PaymentSuccessFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_payment_success, container, false)
+    }
+}
