c82939aea17a0d4b13bcd7db81ce81c5f25f9ae5
Imam Robani <imam.robani@sicepat.com>
2022-10-02 22:14:19 +0700

    chore: integrate api login and add progressbar lottie

--- app/src/main/java/com/imrob/foodmarket/ui/auth/signin/SigninFragment.kt
+++ app/src/main/java/com/imrob/foodmarket/ui/auth/signin/SigninFragment.kt
@@ -1,17 +1,23 @@
 package com.imrob.foodmarket.ui.auth.signin
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.imrob.foodmarket.R
+import com.imrob.foodmarket.model.response.login.LoginResponse
 import com.imrob.foodmarket.ui.MainActivity
 import com.imrob.foodmarket.ui.auth.AuthActivity
 import kotlinx.android.synthetic.main.fragment_signin.*
 
-class SigninFragment : Fragment() {
+class SigninFragment : Fragment(), SigninContract.View {
+
+    lateinit var presenter: SigninPresenter
+    var progressDialog: Dialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,18 +29,48 @@ class SigninFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        presenter = SigninPresenter(this)
 
-        btnSignup.setOnClickListener{
+        initView()
+
+        btnSignup.setOnClickListener {
             val signup = Intent(activity, AuthActivity::class.java)
             signup.putExtra("page_request", 2)
             startActivity(signup)
         }
 
         btnSignin.setOnClickListener {
-            val home = Intent(activity, MainActivity::class.java)
-            startActivity(home)
-            activity?.finish()
+            presenter.submitLogin("imamrob@gmail.com", "12345678")
         }
+    }
+
+    override fun onLoginSuccess(loginResponse: LoginResponse) {
+        val home = Intent(activity, MainActivity::class.java)
+        startActivity(home)
+        activity?.finish()
+    }
+
+    override fun onLoginFailed(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initView() {
+        progressDialog = Dialog(requireContext())
+        val dialogLayout = layoutInflater.inflate(R.layout.dialog_loader, null)
+
+        progressDialog?.let {
+            it.setContentView(dialogLayout)
+            it.setCancelable(false)
+            it.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        }
+    }
+
+    override fun showLoading() {
+        progressDialog?.show()
+    }
+
+    override fun dismissLoading() {
+        progressDialog?.dismiss()
     }
 
 }
--- app/src/main/java/com/imrob/foodmarket/ui/auth/signin/SigninPresenter.kt
+++ app/src/main/java/com/imrob/foodmarket/ui/auth/signin/SigninPresenter.kt
@@ -1,16 +1,36 @@
 package com.imrob.foodmarket.ui.auth.signin
 
+import com.imrob.foodmarket.network.HttpClient
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class SigninPresenter(private val view: SigninContract.View) : SigninContract.Presenter {
 
     private val mCompositeDisposable: CompositeDisposable?
+
     init {
         this.mCompositeDisposable = CompositeDisposable()
     }
 
     override fun submitLogin(email: String, password: String) {
-        TODO("Not yet implemented")
+        view.showLoading()
+        val disposable =
+            HttpClient.getInstance().getApi()!!.login(email, password).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(
+                    {
+                        view.dismissLoading()
+                        if (it.meta?.status.equals("success", true)) {
+                            it.data?.let { it1 -> view.onLoginSuccess(it1) }
+                        } else {
+                            it.meta?.message?.let { it1 -> view.onLoginFailed(it1) }
+                        }
+                    }, {
+                        view.dismissLoading()
+                        view.onLoginFailed(it.message.toString())
+                    }
+                )
+        mCompositeDisposable!!.add(disposable)
     }
 
     override fun subscribe() {
