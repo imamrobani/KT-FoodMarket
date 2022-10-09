f8787346650719886ec7d984c0c779fa0c986a30
Imam Robani <imamrob@gmail.com>
2022-10-09 22:21:48 +0700

    chore: setup camera signup page

--- app/src/main/java/com/imrob/foodmarket/ui/auth/signup/SignupFragment.kt
+++ app/src/main/java/com/imrob/foodmarket/ui/auth/signup/SignupFragment.kt
@@ -1,17 +1,26 @@
 package com.imrob.foodmarket.ui.auth.signup
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.github.dhaval2404.imagepicker.ImagePicker
 import com.imrob.foodmarket.R
 import com.imrob.foodmarket.ui.auth.AuthActivity
 import kotlinx.android.synthetic.main.fragment_signup.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class SignupFragment : Fragment() {
+
+    var filePath: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,11 +33,45 @@ class SignupFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        initDummy()
+        initListener()
+
         btnContinue.setOnClickListener {
             Navigation.findNavController(it)
                 .navigate(R.id.action_signup_address, null)
 
             (activity as AuthActivity).toolbarSignupAdress()
+        }
+    }
+
+    private fun initListener() {
+        ivProfile.setOnClickListener {
+            ImagePicker.with(this)
+                .cameraOnly()
+                .start()
+        }
+    }
+
+    private fun initDummy() {
+        etFullname.setText("Imam Robani")
+        etEmail.setText("imamrob@gmail.com")
+        etPassword.setText("password")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            filePath = data?.data
+
+            Glide.with(this)
+                .load(filePath)
+                .apply(RequestOptions.circleCropTransform())
+                .into(ivProfile)
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(context, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Task Cancelled", Toast.LENGTH_SHORT).show()
         }
     }
 }
